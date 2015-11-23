9c277095e3c84b217092aec1e774250f2a1bc49c
AlianOL <522070742@qq.com>
2015-11-23 19:59:27 +0800

    hahahah

---
+++ WeiBoAPP/WeiBoAPP/日了狗了_搞了一晚上.h
@@ -0,0 +1,13 @@
+//
+//  日了狗了_搞了一晚上.h
+//  WeiBoAPP
+//
+//  Created by Ibokan2 on 15/11/23.
+//  Copyright © 2015年 Ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface __________ : UIViewController
+
+@end
---
+++ WeiBoAPP/WeiBoAPP/日了狗了_搞了一晚上.m
@@ -0,0 +1,37 @@
+//
+//  日了狗了_搞了一晚上.m
+//  WeiBoAPP
+//
+//  Created by Ibokan2 on 15/11/23.
+//  Copyright © 2015年 Ibokan. All rights reserved.
+//
+
+#import "日了狗了_搞了一晚上.h"
+
+@interface __________ ()
+
+@end
+
+@implementation __________
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
